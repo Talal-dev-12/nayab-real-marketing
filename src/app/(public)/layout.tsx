59f465e94b2ad010dbda10c509d7e4fddae303e4
Talal-dev-12 <m.tallal.nadeem@gmail.com>
2026-04-06 00:21:53 +0500

    insight

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export const metadata: Metadata = {
@@ -78,6 +79,7 @@ export default function RootLayout({
       <body suppressHydrationWarning>
         <Navbar />
         {children}
+        <SpeedInsights/>
            <Footer />
            </body>
     </html>
