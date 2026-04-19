47f3d012a57e9e7dbb173115b7bf48f63bf4c9e9
Talal-dev-12 <m.tallal.nadeem@gmail.com>
2026-04-20 02:08:04 +0500

    feat: implement root layout with SEO metadata, structured data, and analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata: Metadata = {
   title:
     "Nayab Real Marketing | Properties for Sale in Karachi | Plots & Houses",
@@ -77,7 +78,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body suppressHydrationWarning><Analytics />{children}</body>
+      <body suppressHydrationWarning><Analytics /><SpeedInsights />{children}</body>
     </html>
   );
 }
